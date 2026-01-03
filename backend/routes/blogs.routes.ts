691aa12951b7d9bfb411a8885abcb57584270f0d
TyPhu0312 <nhokty12343@gmail.com>
2026-01-03 10:47:24 +0700

    BE tạo bài viết và duyệt bài viết admin

--- backend/routes/blogs.routes.ts
+++ backend/routes/blogs.routes.ts
@@ -2,7 +2,10 @@ import express from "express";
 import {
   getBlogs,
   getBlogById,
+  getBlogByUserId,
   createBlog,
+  approveBlog,
+  rejectBlog,
   updateBlog,
   deleteBlog,
 } from "../controllers/blogs.controller";
@@ -12,7 +15,10 @@ const router = express.Router();
 
 router.get("/", getBlogs);
 router.get("/:id", getBlogById);
+router.get("/user/:userId", getBlogByUserId);
 router.post("/create", uploadBlogImage.single("image"), createBlog);
+router.post("/:id/approve", approveBlog);
+router.post("/:id/reject", rejectBlog);
 router.put("/update/:id", uploadBlogImage.single("image"), updateBlog);
 router.delete("/delete/:id", deleteBlog);
 
